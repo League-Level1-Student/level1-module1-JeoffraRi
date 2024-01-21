93c9038596276f9036c32b065c71fde8d467ab67
AmtrakFTW <88908613+JeoffraRi@users.noreply.github.com>
2024-01-21 14:30:15 -0800

    gewdwdqw
    fedxqd

---
+++ src/_11_binary_converter/Bin.java
@@ -0,0 +1,19 @@
+package _11_binary_converter;
+
+import javax.swing.JFrame;
+
+public class Bin {
+public static void main(String[] args) {
+	Bin bin = new Bin();
+}
+
+
+Bin() {
+	JFrame frame = new JFrame();
+	frame.setVisible(true);
+}
+
+
+
+
+}
