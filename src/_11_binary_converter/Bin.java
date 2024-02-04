c2bf53d67997c982464ec38fe8c23faafed323f4
AmtrakFTW <88908613+JeoffraRi@users.noreply.github.com>
2024-02-04 14:14:04 -0800

    save some
    for the whales

--- src/_11_binary_converter/Bin.java
+++ src/_11_binary_converter/Bin.java
@@ -1,17 +1,65 @@
 package _11_binary_converter;
 
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Bin {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Bin implements ActionListener {
 public static void main(String[] args) {
 	Bin bin = new Bin();
 }
 
 
+JTextField bigBird = new JTextField(10);
+
 Bin() {
 	JFrame frame = new JFrame();
 	frame.setVisible(true);
+	JPanel panel = new JPanel();
+	JButton button = new JButton("Pingu");
+	panel.add(button);
+	frame.add(panel);
+	button.addActionListener(this);
+	panel.add(bigBird);
+	frame.pack();
+	
 }
+
+String convert(String input) {
+    if(input.length() != 8){
+        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+        return "-";
+    }
+    String binary = "[0-1]+";//must contain numbers in the given range
+    if (!input.matches(binary)) {
+        JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+        return "-";   
+    }
+    try {
+        int asciiValue = Integer.parseInt(input, 2);
+        char theLetter = (char) asciiValue;
+        return "" + theLetter;
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+        return "-";
+    }
+}
+
+
+@Override
+public void actionPerformed(ActionEvent e) {
+
+	
+	String elmo = convert(bigBird.getText());
+	System.out.println(elmo);
+}
+
+           
 
 
 
